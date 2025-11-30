fcecfb9ea87dd46305e28bd4b5479545b632a9d2
Simon Winder <greengekko333@gmail.com>
2025-11-30 23:49:09 +1300

    solve day 8 part 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -27,12 +27,34 @@ fn decode(line: &str) -> String {
     out
 }
 
+/**
+ * Lazy counting of new encoding / characters for part 2
+ */
+fn count_encoded_line(line: &str) -> usize {
+    let mut count = 6; // for wrapped "" we dropped off
+    let mut chars = line.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        if c == '\\' {
+            count += 1;
+        }
+        if c == '"' {
+            count += 1;
+        }
+
+        count += 1;
+    }
+
+    count
+}
+
 fn main() {
     let list = fs::read_to_string("list.txt")
         .expect("Should have been able to read list.txt");
 
     let mut string_lit = 0;
     let mut chars = 0;
+    let mut encoded_count = 0;
 
     for line in list.lines() {
         chars += line.len();
@@ -40,9 +62,14 @@ fn main() {
         let _line = &line[1..line.len() - 1];
         let decoded = decode(_line);
         string_lit += decoded.len();
+
+        let encoded_line_count = count_encoded_line(_line);
+        encoded_count += encoded_line_count;
     }
 
-    let answer = chars - string_lit;
+    let part1 = chars - string_lit;
+    let part2 = encoded_count - chars;
 
-    println!("{}", answer);
+    println!("{}", part1);
+    println!("{}", part2);
 }
